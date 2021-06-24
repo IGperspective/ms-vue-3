d3c62162d0f99b6d31f9024e3fe2beefda01f055
IGperspective <73352028+IGperspective@users.noreply.github.com>
2021-06-24 12:58:46 +0100

    dublicated styles removed

--- src/components/MsRestartButton.vue
+++ src/components/MsRestartButton.vue
@@ -15,22 +15,6 @@ export default {};
   height: 40px;
   margin: 8px;
   display: block;
-  background: url(../../src/img/ship33.svg);
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: 25px;
-  border-radius: 50%;
-  background-color: var(--ms-light-blue);
-  border: 3px inset var(--cell-border--closed);
-}
-.ms-restart-button {
-  outline: none;
-  border: none;
-  padding: 0px;
-  width: 40px;
-  height: 40px;
-  margin: 8px;
-  display: block;
   background: url(../src/img/ship33.svg);
   background-repeat: no-repeat;
   background-position: center;
